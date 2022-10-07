5bf48a5eb329965e1db552bffcfa976bc58cdd5f
AlejoFernandez10 <alejofernandez1239@gmail.com>
2022-10-06 21:22:35 -0300

    Estructura terminada

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let animado = document.querySelectorAll(`.animado`);
+
+const mostrar = () =>{
+    let scrollTop = document.documentElement.scrollTop;
+
+    for(let i = 0; i < animado.length; i++){
+
+        let alturaAnimado = animado[i].offsetTop;
+        if(0 = 0){
+
+        }
+    };
+}
